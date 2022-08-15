d55dec7f16e95b1ee6e82c8716a0e670ee314964
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2022-08-15 08:38:56 +0300

    Add files via upload

--- VendingMachine.sol
+++ VendingMachine.sol
@@ -34,7 +34,7 @@ function urun_isim(uint urun_no) public view returns(string memory urun_adi){
 function urun_stok(uint urun_stok_id) public view returns(uint urun_stok_no){
     return urun_listler[urun_stok_id].urun_stok;
 }
-function satinAl(uint urun_num, uint alma_sayi ) public {
+function satinAl(uint urun_num, uint alma_sayi ) public payable {
     purchase storage sender = SatinAlma[msg.sender];
     sender.urun_index = toplam_urun_sayisi;
     urun_listler[urun_num].urun_stok -= alma_sayi;
@@ -43,5 +43,6 @@ function urun_stok_ekle(uint urun_id ,uint stok_sayi) public {
     
     urun_listler[urun_id].urun_stok += stok_sayi;
 }
+ 
 }
 
